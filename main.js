f4814a7bb10d88b65b071085bb49e657281bb6f9
Diogo Piccirillo <di060.h@gmail.com>
2017-03-31 13:19:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var bab = '',
     baba = '',
     Pi = Math.PI,
     e = Math.E;
-
 function sen(x) {
     return Math.sin(x/180*Pi);
 }
@@ -15,12 +14,21 @@ function tan(x) {
 function ln(x) {
     return Math.log(x);
 }
-function pow(x, y) {
-    return Math.pow(x, y);
-}
-function raiz(x, y) {
-    return Math.pow(x, 1/y);
-}
+Object.defineProperty(Number.prototype, 'pow', {
+    value: function(that) {
+        return Math.pow(this, that);
+    }
+})
+Object.defineProperty(Number.prototype, 'rai', {
+    value: function(that) {
+        return Math.pow(that, 1/this);
+    }
+})
+Object.defineProperty(Number.prototype, 'log', {
+    value: function(that) {
+        return Math.log(that) / Math.log(this);
+    }
+})
 function abs(x) {
     return Math.abs(x);
 }
@@ -30,13 +38,16 @@ function piso(x) {
 function teto(x) {
     return Math.ceil(x);
 }
-function log(x, y) {
-    return Math.log(x) / Math.log(y);
+function rad(x) {
+    return x * (Pi / 180);
 }
-
 function update() {
     baba = bab.replace(/Pi/g, '&pi;');
-    document.getElementById('birl').innerHTML = baba;
+    baba = baba.replace(/ .pow/g, '^');
+    baba = baba.replace(/ .rai/g, '&radic;');
+    baba = baba.replace(/ .log/g, 'log');
+    document.getElementById('for').innerHTML = baba;
+    document.getElementById("res").style.left = String(event.accelerationIncludingGravity.x * 10) + 'px;';
 }
 
 function set(x) {
@@ -44,25 +55,38 @@ function set(x) {
     update();
 }
 function rem() {
-    if (bab.slice(bab.length-3, bab.length) == 'sen' || bab.slice(bab.length-3, bab.length) == 'cos' || bab.slice(bab.length-3, bab.length) == 'tan' || bab.slice(bab.length-3, bab.length) == 'log' || bab.slice(bab.length-3, bab.length) == 'pow' || bab.slice(bab.length-3, bab.length) == 'abs') {
+    if (bab.slice(bab.length-3, bab.length) == 'sen' || bab.slice(bab.length-3, bab.length) == 'cos' || bab.slice(bab.length-3, bab.length) == 'tan' || bab.slice(bab.length-3, bab.length) == 'mod' || bab.slice(bab.length-3, bab.length) == 'abs') {
         bab = bab.substring(0, bab.length-3);
     } else if (bab.slice(bab.length-2, bab.length) == 'ln' || bab.slice(bab.length-2, bab.length) == 'Pi') {
         bab = bab.substring(0, bab.length-2);
     } else if (bab.slice(bab.length-4, bab.length) == 'teto' || bab.slice(bab.length-4, bab.length) == 'piso' || bab.slice(bab.length-4, bab.length) == 'raiz') {
         bab = bab.substring(0, bab.length-4);
+    } else if (bab.slice(bab.length-5, bab.length) == ' .pow' || bab.slice(bab.length-5, bab.length) == ' .rai' || bab.slice(bab.length-5, bab.length) == ' .log') {
+        bab = bab.substring(0, bab.length-5);
     } else {
         bab = bab.substring(0, bab.length-1);
     }
+    document.getElementById('res').innerHTML = "&nbsp;";
     update();
 }
 function ac() {
     bab = '';
-    update();
+    document.getElementById('for').innerHTML = "&nbsp;";
+    document.getElementById('res').innerHTML = "&nbsp;";
+    equal();
 }
 function equal() {
-    if (String(eval(bab)).indexOf('function') == -1) {
-        baba = bab.replace(/Pi/g, '&pi;');
-        document.getElementById('birl').innerHTML = baba + ' = ' + (eval(bab));
-        bab = '';
+    dah = bab.replace(/\+ .rai/g, '+2 .rai');
+    dah = dah.replace(/- .rai/g, '-2 .rai');
+    dah = dah.replace(/\/ .rai/g, '/2 .rai');
+    dah = dah.replace(/\* .rai/g, '*2 .rai');
+    dah = dah.replace(/, .rai/g, ',2 .rai');
+    dah = dah.replace(/\( .rai/g, '(2 .rai');
+    dah = dah.replace(/mod/g, '%');
+//    alert(dah);
+    if (dah.indexOf(' .rai') == 0) dah = dah.replace(" .rai", "2 .rai");
+    if (String(eval(dah)).indexOf('function') == -1 && String(eval(dah)).indexOf('undefined') == -1) {
+        document.getElementById('res').innerHTML = (eval(dah));
+        update();
     }
 }
